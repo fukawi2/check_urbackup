7eeb49f7501ccbf90b887dde3e512c6ba80b77c3
Phillip Smith <phillip.smith@naturesorganics.com.au>
2017-05-08 13:41:57 +1000

    update copyright

--- src/check_urbackup.sh
+++ src/check_urbackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ###############################################################################
-# Copyright (C) 2014 Phillip Smith
+# Copyright (C) 2014-2017 Phillip Smith
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
